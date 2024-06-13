d4b40765d8537c11c983e68491fc6699689a026c
CapybaraSwim <ilya-golovin-03@mail.ru>
2024-06-13 19:04:07 +0300

    header + footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div>
-      Shop 
+    <div className="wrapper">
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Clothes Club</span>
+            <ul className="nav">
+              <li>О нас</li>
+              <li>Контакты</li>
+              <li>Личный кабинет</li>
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
