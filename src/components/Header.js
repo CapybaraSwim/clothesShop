c9b625c9826c3dc5d14728745bb8b7cef775f68b
CapybaraSwim <ilya-golovin-03@mail.ru>
2024-06-19 16:20:37 +0300

    shop-cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {useState} from "react"
+import { FaBasketShopping } from "react-icons/fa6";
 
-export default function header() {
+export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
         <div>
-            <span className='logo'>Clothes Club</span>
-            <ul className='nav'>
+            <span className="logo">Clothes Club</span>
+            <ul className="nav">
               <li>О нас</li>
               <li>Контакты</li>
               <li>Личный кабинет</li>
             </ul>
+            <FaBasketShopping onClick={()=> setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && "active"}`}/>
+
+            {cartOpen && (
+              <div className="shop-cart"></div>
+            )}        
         </div>
-        <div className='presentation'></div>
+        <div className="presentation"></div>
     </header>
   )
 }
